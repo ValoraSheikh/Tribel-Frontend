cc222eb6ec0c3164d053231b6812c1cab1f77efd
Aman Sheikh <haarishsheikh04@gmail.com>
2026-04-09 21:44:16 +0530

    Create e2e test for login auth.setup.ts

---
+++ tests/auth.setup.ts
@@ -0,0 +1,24 @@
+import { test as setup, expect } from "@playwright/test";
+
+const authFile = "playwright/.auth/user.json";
+
+setup("authenticate via Auth0", async ({ page }) => {
+  await page.goto("http://localhost:3001/login");
+
+  await page.waitForURL(/.*auth0\.com.*/);
+
+  await page
+    .getByLabel("Username or Email address")
+    .fill("xevowic351@izeao.com");
+  await page.locator('input[name="password"]').fill("maekjrl234#A");
+  await page.getByRole("button", { name: "Continue", exact: true }).click();
+
+  await page.getByRole("button", { name: "Accept", exact: true }).click();
+
+  await page.waitForURL("http://localhost:3001/");
+  await page.goto(
+    "http://localhost:3001/property/32825c3b-47ec-493c-947e-64046ad9c815",
+  );
+
+  await page.context().storageState({ path: authFile });
+});
