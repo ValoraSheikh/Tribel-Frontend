6f87b1c9143971c32388e792e986aefc952ff582
Aman Sheikh <haarishsheikh04@gmail.com>
2026-04-09 21:50:47 +0530

    Make changes in toast for edit property

--- src/features/property/components/dashboard/edit-property.tsx
+++ src/features/property/components/dashboard/edit-property.tsx
@@ -176,20 +176,7 @@ export function EditPropertyForm({ propertyId }: PropertyIdProps) {
   function onSubmit(data: z.infer<typeof formSchema>) {
     updateProperty.mutate(data, {
       onSuccess: () => {
-        toast("You submitted the following values:", {
-          description: (
-            <pre className="bg-code text-muted-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-              <code>{JSON.stringify(data, null, 2)}</code>
-            </pre>
-          ),
-          position: "bottom-right",
-          classNames: {
-            content: "flex flex-col gap-2",
-          },
-          style: {
-            "--border-radius": "calc(var(--radius)  + 4px)",
-          } as React.CSSProperties,
-        });
+        toast.success("Property updated successfully");
 
         router.push("/properties");
       },
