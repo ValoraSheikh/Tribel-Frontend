b29e0604af9c3bc84f7db93111839009b4aa3cee
Aman Sheikh <haarishsheikh04@gmail.com>
2026-04-17 16:12:38 +0530

    Created api for upload endpoint

---
+++ src/features/upload/api/upload.api.ts
@@ -0,0 +1,36 @@
+import { axiosClient } from "@/lib/axios/axios-client";
+
+export interface UploadProps {
+  entity: string;
+  fileType: string;
+  entityId: string;
+}
+
+export interface FileProps {
+  url: string;
+  file: File;
+}
+
+interface UploadResponse {
+  data: UploadURLProps
+}
+
+interface UploadURLProps{
+  uploadUrl: string;
+  key: string;
+}
+
+export const uploadApi = {
+  uploadUrl: async (payload: UploadProps) => {
+    const { data } = await axiosClient.post<UploadResponse>("/api/v1/uploads/presign", payload);
+    return data.data;
+  },
+
+  uploadFile: async ({ url, file }: FileProps) => {
+    await axiosClient.put(url, file, {
+      headers: {
+        "Content-Type": file.type
+      }
+    });
+  },
+};
