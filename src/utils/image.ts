44d0a97b4212716bf7493f1daa99e45de7bc47db
Aman Sheikh <haarishsheikh04@gmail.com>
2026-04-18 16:36:35 +0530

    Put imageUrl in string case

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,9 +1,9 @@
 const baseUrl = process.env.NEXT_PUBLIC_S3_BUCKET_URL;
 
 export function toUrl(imageUrl: string) {
-  if (imageUrl.startsWith("public/")) {
-    return `${baseUrl}${imageUrl}`
-  } else if (imageUrl.startsWith("http")){
-    return imageUrl
+  if (String(imageUrl).startsWith("public/")) {
+    return `${baseUrl}${imageUrl}`;
+  } else if (String(imageUrl).startsWith("http")) {
+    return imageUrl;
   }
 }
