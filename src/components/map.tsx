66439475cd55dcf8bc8cc4445ea35b1f73e8a01e
ValoraSheikh <haarishsheikh04@gmail.com>
2026-06-10 21:07:01 +0530

    Create property map component for property profile

---
+++ src/components/map.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+
+type PropertyMapProps = {
+  lat: number;
+  lng: number;
+  zoom?: number;
+  height?: string;
+};
+
+export default function PropertyMap({
+  lat,
+  lng,
+  zoom = 15,
+  height = "300px",
+}: PropertyMapProps) {
+  const position = { lat, lng };
+
+  return (
+    <div
+      className="w-full rounded-xl overflow-hidden border"
+      style={{ height }}
+    >
+      <APIProvider
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}
+      >
+        <Map
+          style={{ width: "100%", height: "100%" }}
+          defaultCenter={position}
+          defaultZoom={zoom}
+          gestureHandling="greedy"
+          disableDefaultUI={false}
+        >
+          <AdvancedMarker position={position}>
+            <Pin
+              background="#111827"
+              glyphColor="#ffffff"
+              borderColor="#ffffff"
+            />
+          </AdvancedMarker>
+        </Map>
+      </APIProvider>
+    </div>
+  );
+}
