c3832ff5608e18dd65103ad813d7076d247c38fd
ValoraSheikh <haarishsheikh04@gmail.com>
2026-09-10 16:38:40 +0530

    Support avatar click handlers

--- src/features/profile/components/avatar.tsx
+++ src/features/profile/components/avatar.tsx
@@ -1,10 +1,16 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 
-function AvatarImg({ avatar }: { avatar: string }) {
+function AvatarImg({
+  avatar,
+  onClick,
+}: {
+  avatar: string;
+  onClick?: () => void;
+}) {
   return (
     <div>
-      <Link href="/profile">
+      <Link href="/profile" onClick={onClick}>
         <Avatar className="h-10 w-10">
           <AvatarImage src={avatar} />
         </Avatar>
